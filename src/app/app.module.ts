9575d1662c520a226b546bacbd61ffa82c620da1
salma-k995 <salmakallala@gmail.com>
2021-04-21 12:10:31 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { FormsModule } from '@angular/forms';
+import { DeleteSpacePipe } from './pipes/delete-space.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BooksListComponent,
+    DeleteSpacePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
